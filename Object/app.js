575cdc9de3c6db799d32a39d5f8e155d395ce42d
Work <work@192-168-1-104.tpgi.com.au>
2016-11-13 00:29:51 +1100

    Create object directory and js file

---
+++ Object/app.js
@@ -0,0 +1,18 @@
+function Person(firstname, lastname) {
+    this.firstname = firstname
+    this.lastname = lastname
+}
+
+Person.prototype.greet = function() {
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname)
+}
+
+var john = new Person('John','Doe')
+
+console.log(john.firstname)
+
+
+var patrick = new Person('Patrick','Gao')
+
+patrick.greet
+
