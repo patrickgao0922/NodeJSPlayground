c413bf7f109b25e106171735a3fc33d6b6e9d782
Work <work@192-168-1-104.tpgi.com.au>
2016-11-13 16:36:55 +1100

    ModulePatterns -- Finish

--- ModulePatterns/app.js
+++ ModulePatterns/app.js
@@ -4,6 +4,7 @@ greet()
 var greet2 = require('./greet2.js').greet
 greet2()
 
+// Exports return a Object: the object is cached
 var greet3 = require('./greet3')
 greet3.greet()
 
@@ -11,10 +12,11 @@ greet3.greeting = 'Changed hello world!'
 var greet3b = require('./greet3')
 greet3b.greet()
 
-
+// Exports return a constructor
 var Greet4 = require('./greet4')
 greet4 = new Greet4()
 greet4.greet()
 
+// Revealing Module Patterns
 var greet5 = require('./greet5')
-greet5.greet
+greet5.greet()
