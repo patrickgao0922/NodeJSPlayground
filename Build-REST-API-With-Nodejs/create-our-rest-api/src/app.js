c8401ea664a375c4224a7444398a37a4d6aefe92
Work <patrickgao0922@gmail.com>
2016-11-22 00:00:18 +1100

    index.js

--- Build-REST-API-With-Nodejs/create-our-rest-api/src/app.js
+++
@@ -1,8 +0,0 @@
-import http from 'http'
-import express from 'express'
-import bodyParser from 'bodyParser'
-import mongoose from 'mongoose'
-
-import config from './config'
-import routes from '.routes'
-
---
+++ Build-REST-API-With-Nodejs/create-our-rest-api/src/index.js
@@ -0,0 +1,21 @@
+import http from 'http'
+import express from 'express'
+import bodyParser from 'bodyParser'
+import mongoose from 'mongoose'
+
+import config from './config'
+import routes from '.routes'
+
+let app = express()
+app.server = http:createServer(app)
+
+// middleware
+
+// passport config
+
+// api routes v1
+app.use('/v1',routes)
+app.server.listen(config.port)
+console.log('Started on port ${app.server.address().port}')
+
+export default app
