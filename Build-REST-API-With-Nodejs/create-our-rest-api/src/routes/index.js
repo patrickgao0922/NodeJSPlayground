d8f0f3527b43871fbd57cb96eb4dcd1329b0657a
Work <patrickgao0922@gmail.com>
2016-11-22 00:32:20 +1100

    commit

--- Build-REST-API-With-Nodejs/create-our-rest-api/src/routes/index.js
+++ Build-REST-API-With-Nodejs/create-our-rest-api/src/routes/index.js
@@ -2,3 +2,15 @@ import express from 'express'
 import config from '../config'
 import middleware from '../middleware'
 import initializedDb from '../Db'
+
+let router = express()
+
+// connect to Db
+initializedDb(db => {
+    // internal middleware
+    app.use(middleware({config,db}))
+    // api routes v1 (/v1)
+
+})
+
+export default router
