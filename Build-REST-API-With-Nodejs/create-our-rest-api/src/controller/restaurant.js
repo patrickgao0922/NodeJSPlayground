eb7ec9a61226f23b5791c9d740dd0c2cde3089eb
Patrick Gao <patrickgao0922@gmail.com>
2016-12-01 00:07:53 +1100

    Create first post uri

--- Build-REST-API-With-Nodejs/create-our-rest-api/src/controller/restaurant.js
+++ Build-REST-API-With-Nodejs/create-our-rest-api/src/controller/restaurant.js
@@ -5,4 +5,18 @@ import Restaurant from '../model/restaurant'
 export default({config, db}) => {
     let api = Router()
     // '/v1/restaruant/add'
+    api.post('/add', (res, req) => {
+        let newRest = new Restaurant()
+        newRest.name = req.body.name
+
+        // Use mongoose to work with rest api
+        newRest.save(err => {
+            if (err) {
+                res.send(err)
+            }
+            res.json({message: 'Restaurant saved sucessfully'})
+        })
+    })
+
+    return api
 }
