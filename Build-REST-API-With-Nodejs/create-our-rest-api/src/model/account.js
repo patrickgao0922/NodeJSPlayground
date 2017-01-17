7e201666ecdf1135ccac1bdb24fc14ad1530d3db
Patrick Gao <patrickgao0922@gmail.com>
2017-01-17 22:53:02 +1100

    Finish creating account module

--- Build-REST-API-With-Nodejs/create-our-rest-api/src/model/account.js
+++ Build-REST-API-With-Nodejs/create-our-rest-api/src/model/account.js
@@ -1,3 +1,17 @@
-import mongooss from 'mongoose'
+import mongoose from 'mongoose'
 const Schema = mongoos.Schema
 import passportLocalMongoose from 'passport-local-mongoose'
+
+let Account = new Schema({
+    email: {
+        type:String,
+        requried:true
+    },
+    password: {
+        type:String,
+        required: true
+    }
+})
+
+Account.plugin(passprotLocalMongoose)
+modul.exports = mongoose.model('Account', Account)
