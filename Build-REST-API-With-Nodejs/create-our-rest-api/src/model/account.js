ba1cdc9286b28f813f97b3f32af49ab223bd5cc5
Patrick Gao <patrickgao0922@gmail.com>
2017-01-23 00:48:47 +1100

    Took out required out of account model

--- Build-REST-API-With-Nodejs/create-our-rest-api/src/model/account.js
+++ Build-REST-API-With-Nodejs/create-our-rest-api/src/model/account.js
@@ -3,14 +3,16 @@ const Schema = mongoose.Schema
 import passportLocalMongoose from 'passport-local-mongoose'
 
 let Account = new Schema({
-    email: {
-        type:String,
-        requried:true
-    },
-    password: {
-        type:String,
-        required: true
-    }
+    // email: {
+    //     type:String,
+    //     requried:true
+    // },
+    // password: {
+    //     type:String,
+    //     required: true
+    // }
+    email: String,
+    password: String
 })
 
 Account.plugin(passportLocalMongoose)
